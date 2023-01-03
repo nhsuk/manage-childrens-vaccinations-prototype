20c9983b0e9afefe8853714d67ac038eb8a10d2b
Paul Hayes <fofr@users.noreply.github.com>
2023-01-03 15:51:15 +0000

    Incorporate correct ages for vaccines
    - Primary school, reception to year 6, age 4 to 11 for flu
    - Secondary school, year 8 to 9, age 12 to 14 for HPV

--- app/generators/campaign.js
+++ app/generators/campaign.js
@@ -10,12 +10,33 @@ const generateRandomString = (length) => {
   return Math.random().toString(36).substr(2, length).toUpperCase()
 }
 
+const yearGroups = (type) => {
+  switch (type) {
+    case 'Flu':
+      return [
+        'Reception',
+        'Year 1',
+        'Year 2',
+        'Year 3',
+        'Year 4',
+        'Year 5',
+        'Year 6'
+      ]
+    case 'HPV':
+      return [
+        'Year 8',
+        'Year 9'
+      ]
+  }
+}
+
 export const campaign = (options) => {
   const type = faker.helpers.arrayElement(['Flu', 'HPV'])
   const vaccinesObject = vaccines(faker, type)
-  const schoolObject = school(faker)
+  const schoolObject = school(faker, type)
   const atTime = faker.helpers.arrayElement(['09:00', '10:00', '11:00', '12:30', '13:00', '14:00'])
   const daysUntil = faker.datatype.number({ min: 2, max: 100 })
+  const ageRange = type === 'Flu' ? { minAge: 4, maxAge: 11 } : { minAge: 12, maxAge: 14 }
 
   return {
     id: generateRandomString(3),
@@ -23,8 +44,9 @@ export const campaign = (options) => {
     location: schoolObject.name,
     date: DateTime.now().plus({ days: daysUntil }).toISODate() + 'T' + atTime,
     type,
+    yearGroups: yearGroups(type),
     vaccines: vaccinesObject,
     school: schoolObject,
-    patients: patients({ count: 100, patient: { minAge: 4, maxAge: 11 } })
+    patients: patients({ count: 100, patient: ageRange })
   }
 }
