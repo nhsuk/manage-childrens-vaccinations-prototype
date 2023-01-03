9857265c543583ad317f1274924ff612ba782164
Paul Hayes <fofr@users.noreply.github.com>
2023-01-03 16:13:58 +0000

    Remove old campaigns

--- app/data.js
+++ app/data.js
@@ -15,34 +15,6 @@ export default {
     email: 'jane.doe@example.com'
   },
   campaigns: campaigns({ count: 20 }),
-  campaigns_old: {
-    flu: {
-      id: 'flu',
-      title: 'Flu campaign at St Mary’s Primary School',
-      location: 'St Mary’s Primary School',
-      type: 'flu',
-      vaccine: {
-        brand: 'Fluenz Tetra',
-        method: 'Nasal spray',
-        batch: 'P100475581',
-        summary: 'Flu (Fluenz Tetra, P100475581)'
-      },
-      patients: patients({ count: 100, patient: { minAge: 4, maxAge: 11 } })
-    },
-    hpv: {
-      id: 'hpv',
-      title: 'HPV campaign at Hele’s Secondary School',
-      location: 'Hele’s Secondary School',
-      type: 'HPV',
-      vaccine: {
-        brand: 'Gardasil 9',
-        method: 'Injection',
-        batch: 'G100475581',
-        summary: 'HPV (Gardasil 9, G100475581)'
-      },
-      patients: patients({ count: 100, patient: { minAge: 12, maxAge: 13 } })
-    }
-  },
   vaccines: {
     fluSpray: {
       type: 'flu',
