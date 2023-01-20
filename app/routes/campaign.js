2963a37bd8e784a78efaa7179c9fa9baa956355c
Paul Hayes <fofr@users.noreply.github.com>
2023-01-20 12:27:25 +0000

    Iterate patient page and vaccination journey
    - Split patient page into partials
    - Add health questions and triage section
    - Remove parent section, put into the about patient section
    - Directly ask if they have had the vaccine for Flu or HPV, shortening the journeys
    - Keep the 3 in 1/MenACWY the same for now

--- app/routes/campaign.js
+++ app/routes/campaign.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import { vaccination } from '../wizards/vaccination.js'
 
 export const campaignRoutes = router => {
   /**
@@ -27,7 +28,14 @@ export const campaignRoutes = router => {
       res.locals.vaccinationRecord = _.get(req.session.data, `vaccination.${req.params.campaignId}.${req.params.nhsNumber}`)
     }
 
+    res.locals.paths = vaccination(req)
     next()
+  })
+
+  router.post([
+    '/campaign/:campaignId/patient/:nhsNumber'
+  ], (req, res) => {
+    res.redirect(res.locals.paths.next)
   })
 
   router.post('/campaign/:campaignId', (req, res, next) => {
