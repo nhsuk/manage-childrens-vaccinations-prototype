3bccc21c40cde9183c016bba385d3d511ee06fcc
Paul Hayes <fofr@users.noreply.github.com>
2023-01-26 16:41:15 +0000

    Allow filtering by year group

--- app/generators/year-groups.js
+++ app/generators/year-groups.js
@@ -2,23 +2,23 @@ export const yearGroups = (type) => {
   switch (type) {
     case 'Flu':
       return [
-        'Reception',
-        'Year 1',
-        'Year 2',
-        'Year 3',
-        'Year 4',
-        'Year 5',
-        'Year 6'
+        { name: 'Reception', number: 0 },
+        { name: 'Year 1', number: 1 },
+        { name: 'Year 2', number: 2 },
+        { name: 'Year 3', number: 3 },
+        { name: 'Year 4', number: 4 },
+        { name: 'Year 5', number: 5 },
+        { name: 'Year 6', number: 6 }
       ]
     case 'HPV':
       return [
-        'Year 8',
-        'Year 9'
+        { name: 'Year 8', number: 8 },
+        { name: 'Year 9', number: 9 }
       ]
     case '3-in-1 and MenACWY':
       return [
-        'Year 9',
-        'Year 10'
+        { name: 'Year 9', number: 9 },
+        { name: 'Year 10', number: 10 }
       ]
   }
 
