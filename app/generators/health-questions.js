bd6653986eac849e48b85709e9ea2b946cc4a2af
Paul Hayes <fofr@users.noreply.github.com>
2023-02-07 16:21:30 +0000

    Enhance health questions with realistic answers

--- app/generators/health-questions.js
+++ app/generators/health-questions.js
@@ -1,4 +1,4 @@
-export const healthQuestions = (type) => {
+export const healthQuestions = (faker, type) => {
   let questions = []
   const allergy = { id: 'allergy', question: 'Does your child have any severe allergies that have led to an anaphylactic reaction?', answer: 'No' }
   const medicalConditions = { id: 'medical-conditions', question: 'Does your child have any existing medical conditions?', answer: 'No' }
@@ -30,9 +30,50 @@ export const healthQuestions = (type) => {
 
   questions.push({ id: 'anything-else', question: 'Is there anything else you think we should know?', answer: 'No' })
 
-  return {
+  return enrichWithRealisticAnswers(faker, type, {
     contraindications: false,
     triageNotes: false,
     questions
+  })
+}
+
+const realisticAnswers = {
+  fainting: {
+    'anything-else': 'My child has a history of fainting after receiving injections.'
+  },
+  badExperience: {
+    'anything-else': 'My child had a bad experience with a vaccine before, I just want to make sure they are comfortable and safe'
+  },
+  badReaction: {
+    'anything-else': 'My child recently had a bad reaction to a different vaccine. I just want to make sure we are extra cautious with this.'
+  },
+  surgery: {
+    'anything-else': 'Our child recently had surgery and is still recovering. We want to make sure it’s safe for them to get the vaccine.'
+  },
+  asthma: {
+    'medical-conditions': 'My child was diagnosed with asthma a few months ago and it has been quite challenging for them. They have struggled with breathing difficulties and have needed to use their inhaler several times a day.',
+    medication: 'My child takes medication every day to manage their asthma'
+  },
+  nutAllergy: {
+    allergy: 'My child has a severe nut allergy and has had an anaphylactic reaction in the past. This is something that is extremely important to me and my husband. We make sure to always have an EpiPen on hand and have educated our child about their allergy.',
+    'medical-conditions': 'My child has a severe nut allergy, which is their only existing medical condition that we are aware of.'
   }
+}
+
+const enrichWithRealisticAnswers = (faker, type, health) => {
+  const triageNeeded = faker.helpers.maybe(() => true, { probability: 1 })
+  if (!triageNeeded) {
+    return
+  }
+
+  const answer = faker.helpers.objectKey(realisticAnswers)
+  health.questions.forEach((q) => {
+    if (realisticAnswers[answer][q.id]) {
+      q.details = realisticAnswers[answer][q.id]
+      q.answer = 'Yes'
+      health.contraindications = true
+    }
+  })
+
+  return health
 }
