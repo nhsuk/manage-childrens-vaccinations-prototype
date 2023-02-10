72e2028c0e07c672604f16f9f244cd0847be60e1
Paul Hayes <fofr@users.noreply.github.com>
2023-02-10 14:35:47 +0000

    Add scenarios for user research

--- app/routes/online-offline.js
+++ app/routes/online-offline.js
@@ -1,30 +1,48 @@
+const goOffline = (session) => {
+  session.offlineUploaded = false
+  session.data.features.offline.on = true
+}
+
+const goOnline = (req, hasAnyOfflineChanges) => {
+  const campaigns = req.session.data.campaigns
+  if (hasAnyOfflineChanges(campaigns)) {
+    Object.values(campaigns)
+      .map(c => c.children)
+      .flat()
+      .filter(child => child.seen.isOffline)
+      .forEach(child => {
+        child.seen.isOffline = false
+      })
+
+    req.session.offlineUploaded = true
+  }
+
+  req.session.data.features.offline.on = false
+}
+
 export const onlineOfflineRoutes = (router, hasAnyOfflineChanges) => {
+  router.post(['/scenario-office', '/scenario-back-online'], (req, res) => {
+    goOnline(req, hasAnyOfflineChanges)
+    res.redirect('/dashboard')
+  })
+
+  router.post('/scenario-offline', (req, res) => {
+    goOffline(req.session)
+    res.redirect('/dashboard')
+  })
+
   router.get('/go-offline', (req, res) => {
-    req.session.offlineUploaded = false
-    req.session.data.features.offline.on = true
+    goOffline(req.session)
     res.redirect('back')
   })
 
-  router.get('/dismiss-upload', (req, res, next) => {
+  router.get('/dismiss-upload', (req, res) => {
     req.session.offlineUploaded = false
     res.redirect('back')
   })
 
   router.get('/go-online', (req, res) => {
-    const campaigns = req.session.data.campaigns
-    if (hasAnyOfflineChanges(campaigns)) {
-      Object.values(campaigns)
-        .map(c => c.children)
-        .flat()
-        .filter(child => child.seen.isOffline)
-        .forEach(child => {
-          child.seen.isOffline = false
-        })
-
-      req.session.offlineUploaded = true
-    }
-
-    req.session.data.features.offline.on = false
+    goOnline(req, hasAnyOfflineChanges)
     res.redirect('back')
   })
 }
