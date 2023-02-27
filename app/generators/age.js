305e7987aae11de7ad1934e706a0fef8149ddfe8
Paul Hayes <fofr@users.noreply.github.com>
2023-02-27 13:30:34 +0000

    Split age, year group, dob and consent into generators

---
+++ app/generators/age.js
@@ -0,0 +1,44 @@
+import { DateTime, Interval } from 'luxon'
+
+const yearGroupRange = (startYear) => {
+  const startDate = DateTime.fromISO(`${startYear}-09-01`)
+  const endDate = DateTime.fromISO(`${startYear + 1}-08-31`)
+  return {
+    start: startDate.toISO(),
+    end: endDate.toISO(),
+    interval: Interval.fromDateTimes(startDate, endDate)
+  }
+}
+
+const year0 = 2017
+const yearGroups = {}
+for (let i = 0; i < 13; i++) {
+  yearGroups[i] = yearGroupRange(year0 - i)
+}
+
+export const dateOfBirth = (faker, options) => {
+  return faker.date.between(
+    yearGroups[options.maxYearGroup].start,
+    yearGroups[options.minYearGroup].end
+  )
+}
+
+export const yearGroup = (dob) => {
+  const dobDate = DateTime.fromJSDate(dob)
+  for (const [yearGroup, range] of Object.entries(yearGroups)) {
+    if (range.interval.contains(dobDate)) {
+      return yearGroup
+    }
+  }
+}
+
+export const age = (dob) => {
+  const today = new Date()
+  const birthDate = new Date(dob)
+  let age = today.getFullYear() - birthDate.getFullYear()
+  const m = today.getMonth() - birthDate.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--
+  }
+  return age
+}
--- app/generators/child.js
+++ app/generators/child.js
@@ -1,92 +1,21 @@
 import { person } from './person.js'
 import { parent } from './parent.js'
 import { address } from './address.js'
+import { consent } from './consent.js'
 import { gp } from './gp.js'
 import { healthQuestions } from './health-questions.js'
-import { DateTime, Interval } from 'luxon'
+import { dateOfBirth, yearGroup, age } from './age.js'
 import { faker } from '@faker-js/faker'
 faker.locale = 'en_GB'
-
-const age = (dob) => {
-  const today = new Date()
-  const birthDate = new Date(dob)
-  let age = today.getFullYear() - birthDate.getFullYear()
-  const m = today.getMonth() - birthDate.getMonth()
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--
-  }
-  return age
-}
-
-const yearGroupRange = (startYear) => {
-  const startDate = DateTime.fromISO(`${startYear}-09-01`)
-  const endDate = DateTime.fromISO(`${startYear + 1}-08-31`)
-  return {
-    start: startDate.toISO(),
-    end: endDate.toISO(),
-    interval: Interval.fromDateTimes(startDate, endDate)
-  }
-}
-
-const year0 = 2017
-const yearGroups = {}
-for (let i = 0; i < 13; i++) {
-  yearGroups[i] = yearGroupRange(year0 - i)
-}
-
-const yearGroup = (dob) => {
-  const dobDate = DateTime.fromJSDate(dob)
-  for (const [yearGroup, range] of Object.entries(yearGroups)) {
-    if (range.interval.contains(dobDate)) {
-      return yearGroup
-    }
-  }
-}
 
 const preferredName = (child) => {
   return `${child.firstName} ${faker.name.lastName()}`
 }
 
-const consent = (type) => {
-  if (type === '3-in-1 and MenACWY') {
-    const yes = { '3-in-1': 'Given', 'men-acwy': 'Given', both: true, text: 'Given', responded: true }
-    const no = { '3-in-1': 'Refused', 'men-acwy': 'Refused', refusedBoth: true, text: 'Refused', responded: true }
-    const unknown = { '3-in-1': 'Unknown', 'men-acwy': 'Unknown', unknown: true, text: 'Unknown', responded: false }
-
-    return faker.helpers.arrayElement(
-      [
-        yes,
-        yes,
-        yes,
-        yes,
-        yes,
-        { '3-in-1': 'Refused', 'men-acwy': 'Given', text: 'Only MenACWY', responded: true },
-        { '3-in-1': 'Given', 'men-acwy': 'Refused', text: 'Only 3-in-1', responded: true },
-        unknown,
-        unknown,
-        unknown,
-        no,
-        no
-      ]
-    )
-  }
-
-  const r = faker.helpers.arrayElement(['Given', 'Given', 'Given', 'Given', 'Unknown', 'Unknown', 'Unknown', 'Refused'])
-  return {
-    [type]: r,
-    responded: r !== 'Unknown'
-  }
-}
-
 export const child = (options) => {
   const isChild = true
   const c = person(faker, isChild)
-  const consentObj = consent(options.type)
-
-  const dob = faker.date.between(
-    yearGroups[options.maxYearGroup].start,
-    yearGroups[options.minYearGroup].end
-  )
+  const dob = dateOfBirth(faker, options)
 
   c.preferredName = faker.helpers.maybe(() => preferredName(c), { probability: 0.1 })
   // https://digital.nhs.uk/services/e-referral-service/document-library/synthetic-data-in-live-environments
@@ -97,7 +26,7 @@ export const child = (options) => {
   c.age = age(dob)
   c.yearGroup = yearGroup(dob)
   c.parentOrGuardian = parent(faker, c.lastName)
-  c.consent = consentObj
+  c.consent = consent(faker, options.type)
   c.outcome = 'No outcome yet'
   c.seen = {}
   c.healthQuestions = healthQuestions(faker, options.type)
