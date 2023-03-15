26a7ddb2897b2e21a7f3714e7212e0676aff11a4
Paul Hayes <fofr@users.noreply.github.com>
2023-03-15 11:16:45 +0000

    Update campaign in progress generator
    Mark consented children as ready for session, assuming that triage has been completed.

--- app/generators/campaign-in-progress.js
+++ app/generators/campaign-in-progress.js
@@ -2,7 +2,7 @@ import campaign from './campaign.js'
 import _ from 'lodash'
 import { DateTime } from 'luxon'
 import { faker } from '@faker-js/faker'
-import { CONSENT, OUTCOME } from '../enums.js'
+import { CONSENT, OUTCOME, TRIAGE } from '../enums.js'
 
 const setOutcome = (child, consent) => {
   if (consent === CONSENT.REFUSED || consent === CONSENT.UNKNOWN) {
@@ -15,11 +15,22 @@ const setOutcome = (child, consent) => {
   }
 }
 
+const setTriageOutcome = (child, consent) => {
+  if (consent === CONSENT.GIVEN) {
+    child.triageStatus = TRIAGE.READY
+  }
+}
+
 export default (options) => {
   const c = campaign(options)
 
   c.inProgress = true
   c.date = DateTime.now().toISODate() + 'T' + '09:00'
+
+  // set triage outcomes for all children
+  c.children.forEach(child => {
+    setTriageOutcome(child, child.consent[c.type])
+  })
 
   _.sampleSize(c.children, 50).forEach(child => {
     setOutcome(child, child.consent[c.type])
