dafcbad0a997715fd6b6b15a9711fd32b0224c40
Paul Hayes <fofr@users.noreply.github.com>
2023-03-16 12:22:32 +0000

    Stub routes and wizard for consent

---
+++ app/routes/consent.js
@@ -0,0 +1,35 @@
+import wizard from '../wizards/consent.js'
+
+export default (router) => {
+  router.all([
+    '/consent/:campaignId/:nhsNumber',
+    '/consent/:campaignId/:nhsNumber/:view'
+  ], (req, res, next) => {
+    const data = req.session.data
+    const campaign = data.campaigns[req.params.campaignId]
+
+    res.locals.campaign = campaign
+    res.locals.child = campaign.children.find(c => c.nhsNumber === req.params.nhsNumber)
+    res.locals.paths = wizard(req)
+    next()
+  })
+
+  router.get([
+    '/consent/:campaignId/:nhsNumber'
+  ], (_req, res) => {
+    res.render('consent/index')
+  })
+
+  router.get([
+    '/consent/:campaignId/:nhsNumber/:view'
+  ], (req, res) => {
+    res.render(`consent/${req.params.view}`)
+  })
+
+  router.post([
+    '/consent/:campaignId/:nhsNumber',
+    '/consent/:campaignId/:nhsNumber/:view'
+  ], (_req, res) => {
+    res.redirect(res.locals.paths.next)
+  })
+}
