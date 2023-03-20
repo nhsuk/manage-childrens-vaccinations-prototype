19ef58cc2faca654899a5c83609059ecc2d259fa
Paul Hayes <fofr@users.noreply.github.com>
2023-03-20 15:58:53 +0000

    Add no consent journey

--- app/wizards/consent.js
+++ app/wizards/consent.js
@@ -3,17 +3,36 @@ import { wizard } from 'nhsuk-prototype-rig'
 export default (req) => {
   const nhsNumber = req.params.nhsNumber
   const campaignId = req.params.campaignId
-  const campaign = req.session.data.campaigns[campaignId]
-  const child = campaign.children.find(p => p.nhsNumber === req.params.nhsNumber)
   const basePath = `/consent/${campaignId}/${nhsNumber}`
+  const baseData = `consent.${campaignId}.${nhsNumber}`
 
   const journey = {
     [`/campaign/${campaignId}/child/${nhsNumber}`]: {},
     [basePath]: {},
-    [`${basePath}/consent`]: {},
-    [`${basePath}/child-details`]: {},
-    [`${basePath}/details`]: {},
-    [`${basePath}/vaccinate`]: {},
+    [`${basePath}/consent`]: {
+      [`${basePath}/health-questions`]: {
+        data: `${baseData}.consent`,
+        excludedValue: 'No'
+      }
+    },
+    [`${basePath}/why-not-consenting`]: {
+      [`${basePath}/confirm`]: true
+    },
+    [`${basePath}/health-questions`]: {
+      [`${basePath}/confirm`]: true
+    },
+    [`${basePath}/confirm`]: {
+      [`/campaign/${campaignId}/child/${nhsNumber}`]: {
+        data: `${baseData}.consent`,
+        value: 'No'
+      }
+    },
+    [`${basePath}/vaccinate`]: {
+      [`/vaccination/${campaignId}/${nhsNumber}/not-given`]: {
+        data: `vaccination.${campaignId}.${nhsNumber}.given`,
+        value: 'No'
+      }
+    },
     [`/vaccination/${campaignId}/${nhsNumber}/details`]: {}
   }
 
