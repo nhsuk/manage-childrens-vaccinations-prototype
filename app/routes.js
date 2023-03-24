bfe1a1c3f37e539ac8a5512b70b18425740fb20c
Paul Hayes <fofr@users.noreply.github.com>
2023-03-24 16:29:08 +0000

    First pass at explicit syncing

--- app/routes.js
+++ app/routes.js
@@ -39,6 +39,8 @@ router.all('*', (req, res, next) => {
   res.locals.offlineUploaded = req.session.offlineUploaded
   res.locals.hasAnyOfflineChanges = hasAnyOfflineChanges(req.session.data.campaigns)
   res.locals.totalOfflineChangesCount = offlineChangesCount(req.session.data.campaigns)
+
+  res.locals.totalOfflineChangesCount = 100
   next()
 })
 
