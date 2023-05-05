86d6ebfed1fea57c564374227754619fa39db7f6
Paul Hayes <fofr@users.noreply.github.com>
2023-05-05 11:44:25 +0100

    Incorporate offline codes into journey

--- app/routes/online-offline.js
+++ app/routes/online-offline.js
@@ -25,7 +25,12 @@ export default (router, hasAnyOfflineChanges) => {
 
   router.post('/scenario-offline', (req, res) => {
     goOffline(req.session)
-    res.redirect('/dashboard')
+
+    if (req.session.data.hasOfflineCode) {
+      res.redirect('/give-offline-code')
+    } else {
+      res.redirect('/dashboard')
+    }
   })
 
   router.get('/synced-ok', (req, _res, next) => {
@@ -42,5 +47,24 @@ export default (router, hasAnyOfflineChanges) => {
   router.get('/go-online', (req, res) => {
     goOnline(req)
     res.redirect('back')
+  })
+
+  router.all('/set-offline-code', (req, res, next) => {
+    const data = req.session.data
+    const campaign = data.campaigns[req.query.campaignId]
+
+    res.locals.campaign = campaign
+    res.locals.campaignId = req.query.campaignId
+    next()
+  })
+
+  router.post('/set-offline-code', (req, res) => {
+    req.session.data.hasOfflineCode = true
+    res.locals.campaign['available-offline'] = true
+    res.redirect(`/campaign/${res.locals.campaignId}`)
+  })
+
+  router.post('/give-offline-code', (req, res) => {
+    res.redirect('/dashboard')
   })
 }
