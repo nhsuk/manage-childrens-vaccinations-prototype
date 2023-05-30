3c7a340124297a38e1e646026762bf893de0f677
Paul Hayes <fofr@users.noreply.github.com>
2023-05-30 14:34:04 +0100

    Allow editing of batches
    First pass

--- app/routes/vaccine-batches.js
+++ app/routes/vaccine-batches.js
@@ -1,5 +1,29 @@
+const mapVaccineData = (data) => {
+  const map = {}
+  data.forEach(vaccine => {
+    vaccine.batches.forEach(batch => {
+      map[batch.name] = {
+        name: batch.name,
+        vaccine: vaccine.vaccine,
+        brand: vaccine.brand,
+        method: vaccine.method,
+        expiry: batch.expiry
+      }
+    })
+  })
+
+  return map
+}
+
 export default (router) => {
   router.get('/vaccines', (req, res, next) => {
     next()
+  })
+
+  router.get('/vaccines/:batchNumber', (req, res) => {
+    const batches = mapVaccineData(req.session.data.vaccines)
+    res.locals.batch = batches[req.params.batchNumber]
+
+    res.render('vaccines/batch')
   })
 }
