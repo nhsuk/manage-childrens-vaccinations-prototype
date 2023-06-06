23ddae573262eeb2d94301388f0b556e1782bf8f
Paul Hayes <fofr@users.noreply.github.com>
2023-06-06 12:46:29 +0100

    Ask for batch during vaccination flow

--- app/wizards/vaccination.js
+++ app/wizards/vaccination.js
@@ -8,6 +8,7 @@ const getData = (data, keyPath) => {
 const journeyForEverythingElse = (data, campaign, child) => {
   const nhsNumber = child.nhsNumber
   const campaignId = campaign.id
+  const hasDefaultBatch = !!data.todaysBatch
   const isUnknown = child.consent.unknown || child.consent[campaign.type] === 'Unknown'
   const refused = child.consent.refusedBoth || child.consent[campaign.type] === 'Refused'
   const hasConsent = !(isUnknown || refused)
@@ -36,6 +37,12 @@ const journeyForEverythingElse = (data, campaign, child) => {
   if (!hasConsent && !askingForConsent) {
     return {
       [`/campaign/${campaignId}/children?noConsent=${nhsNumber}`]: {}
+    }
+  }
+
+  if (!hasDefaultBatch) {
+    return {
+      [`/vaccination/${campaignId}/${nhsNumber}/which-batch`]: {}
     }
   }
 
