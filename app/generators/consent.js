7176da3a5052b7ca4e4b1847819b7bcf349739e8
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-09-22 16:15:47 +0100

    Move child.healthAnswers to child.consentResponse object

--- app/generators/consent.js
+++ app/generators/consent.js
@@ -1,14 +1,18 @@
+import { faker } from '@faker-js/faker'
 import { CONSENT } from '../enums.js'
 
-export default (type, consentResponses) => {
-  const consentResponse = consentResponses[0][type]
+export default (type) => {
+  const consent = faker.helpers.arrayElement([
+    ...Array(10).fill(CONSENT.GIVEN),
+    ...Array(5).fill(CONSENT.UNKNOWN),
+    CONSENT.REFUSED
+  ])
 
   return {
-    [type]: consentResponse,
-    text: consentResponse,
-    refused: consentResponse === CONSENT.REFUSED,
-    consented: consentResponse === CONSENT.GIVEN,
-    responded: consentResponse !== CONSENT.UNKNOWN,
-    answersNeedTriage: consentResponse.answersNeedTriage
+    [type]: consent,
+    text: consent,
+    refused: consent === CONSENT.REFUSED,
+    consented: consent === CONSENT.GIVEN,
+    responded: consent !== CONSENT.UNKNOWN
   }
 }
