5136f6a966e21dba57af12e1194fbfe6a3cff39b
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-23 00:03:56 +0000

    Use lodash to sort campaigns

--- create-data.js
+++ create-data.js
@@ -2,25 +2,16 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import _ from 'lodash'
-import { DateTime } from 'luxon'
 import { faker } from '@faker-js/faker'
 import getSession from './app/fakers/session.js'
 import getSessionInProgress from './app/fakers/session-in-progress.js'
 import getUser from './app/fakers/user.js'
 import getVaccines from './app/fakers/vaccines.js'
 
-// Create sessions, with one in progress
 const sessionsArray = faker.helpers.multiple(getSession, { count: 20 })
-
-// Add in progress sessions
 sessionsArray.push(getSessionInProgress('Flu'))
 sessionsArray.push(getSessionInProgress('HPV'))
-
-// Sort sessions by date
-sessionsArray.sort((a, b) =>
-  DateTime.fromISO(a.date).valueOf() - DateTime.fromISO(b.date).valueOf()
-)
-const sessions = _.keyBy(sessionsArray, 'id')
+const sessions = _.keyBy(_.sortBy(sessionsArray, ['date']), 'id')
 
 const usersArray = faker.helpers.multiple(getUser, { count: 20 })
 const users = _.keyBy(usersArray, 'id')
