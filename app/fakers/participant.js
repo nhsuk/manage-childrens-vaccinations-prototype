6e1d539dfaa2f4751a6e4403773f70dda102a27b
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-28 15:08:15 +0000

    Add full participant data

--- app/fakers/participant.js
+++ app/fakers/participant.js
@@ -1,4 +1,6 @@
 import { fakerEN_GB as faker } from '@faker-js/faker'
+import getAddress from './address.js'
+import getChild from './child.js'
 import getParent from './parent.js'
 
 /**
@@ -14,14 +16,19 @@ import getParent from './parent.js'
  * @returns {Participant} Participant
  */
 export default () => {
-  const firstName = faker.person.firstName()
-  const lastName = faker.person.firstName()
+  const nhsNumber = faker.helpers.replaceSymbolWithNumber('999#######')
+
+  const child = getChild(5, 10)
+  child.address = getAddress()
+  child.nhsNumber = faker.helpers.maybe(() => nhsNumber, { probability: 0.7 })
+  child.urn = faker.helpers.replaceSymbolWithNumber('1####')
+
+  const parent = getParent(child)
 
   const participant = {
     id: faker.string.uuid(),
-    fullName: `${firstName} ${lastName}`,
-    email: faker.internet.email({ firstName, lastName }).toLowerCase(),
-    tel: faker.helpers.replaceSymbolWithNumber('07### ######')
+    parent,
+    child
   }
 
   return participant
