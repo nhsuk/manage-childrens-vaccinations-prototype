2d04c11aaffadbfa779fd2d2922f16b25a289b9f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-01-11 12:02:57 +0000

    Add ability to download participant data as CSV file

--- app/fakers/participant.js
+++ app/fakers/participant.js
@@ -1,7 +1,11 @@
+import fs from 'node:fs'
 import { fakerEN_GB as faker } from '@faker-js/faker'
 import getAddress from './address.js'
 import getChild from './child.js'
 import getParent from './parent.js'
+
+const schoolData = 'app/fakers/seeds/schools.json'
+const schools = JSON.parse(fs.readFileSync(schoolData))
 
 /**
  * @typedef {object} Participant
@@ -23,13 +27,25 @@ export default () => {
   child.nhsNumber = faker.helpers.maybe(() => nhsNumber, { probability: 0.7 })
   child.urn = faker.helpers.arrayElement([100216, 100757, 100961])
 
+  const school = schools.find(
+    school => Number(school.urn) === child.urn
+  )
+
   const parent = getParent(child)
 
   const participant = {
     id: faker.string.uuid(),
     urn: child.urn,
+    schoolName: school.name,
+    submitted: faker.date.recent({ days: 14 }),
     parent,
     child
+  }
+
+  participant.child.address = {
+    line1: child.address.split('\n')[0],
+    locality: child.address.split('\n')[1],
+    postcode: child.address.split('\n')[2]
   }
 
   return participant
